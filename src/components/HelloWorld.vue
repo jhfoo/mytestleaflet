b94993f307c9d59d60b3bc5d2c831a25ee3f8808
Foo JH <foojihaw@hotmail.com>
2020-04-11 18:40:53 +0800

    Simple working leafletjs integration to Vue + Vuetify

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    Lat: {{ map.center.lat }}, Long: {{ map.center.lng}}<br/>
+    Zoom: {{ map.zoom}}<br/>
+    <l-map :zoom="map.zoom" :center="map.center" @update:center="onUpdateMapCenter" @update:zoom="onUpdateMapZoom" id="mymap">
+      <l-tile-layer :url="MapUrl" :attribution="MapAttr"/>
+    </l-map>
+  </div>
+</template>
+
+<style scoped>
+#mymap {
+  height: 500px;
+}
+</style>
+
+<script>
+import { latLng } from 'leaflet'
+import { LMap, 
+  LTileLayer, 
+  // LMarker 
+} from 'vue2-leaflet'
+
+const INITMAP = {
+  zoom: 12,
+  center: latLng(1.3340316734680562, 103.809814453125)
+}
+  export default {
+    name: 'HelloWorld',
+    components: {
+      LMap,
+      LTileLayer,
+      // LMarker,
+    },
+    data: () => ({
+      MapUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      MapOptions: {
+        zoomSnap: 0.5
+      },
+      MapAttr: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      map: {
+        center: INITMAP.center,
+        zoom: INITMAP.zoom
+      }
+    }),
+    mounted() {
+      // L.map('mymap').setView([51.505, -0.09], 13)
+    },
+    methods: {
+      onUpdateMapCenter(NewCenter) {
+        this.map.center = NewCenter
+      },
+      onUpdateMapZoom(NewZoom) {
+        this.map.zoom = NewZoom
+      }
+    }
+  }
+</script>
