fdc127811c591d1ca9e9d4fac5dbc5081470e920
Foo JH <foojihaw@hotmail.com>
2020-04-12 19:17:51 +0800

    NEW: pin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,10 @@
     Zoom: {{ map.zoom}}<br/>
     <l-map :zoom="map.zoom" :center="map.center" @update:center="onUpdateMapCenter" @update:zoom="onUpdateMapZoom" id="mymap">
       <l-tile-layer :url="MapUrl" :attribution="MapAttr"/>
+      <l-marker :lat-lng="marker.latlong" v-for="(marker, index) in pins" v-bind:key="index + '' + marker.text">
+        <l-popup>{{ marker.text }}
+        </l-popup>
+      </l-marker>
     </l-map>
   </div>
 </template>
@@ -18,7 +22,7 @@
 import { latLng } from 'leaflet'
 import { LMap, 
   LTileLayer, 
-  // LMarker 
+  LMarker, LPopup
 } from 'vue2-leaflet'
 
 const INITMAP = {
@@ -30,13 +34,20 @@ const INITMAP = {
     components: {
       LMap,
       LTileLayer,
-      // LMarker,
+      LMarker,
+      LPopup
     },
     data: () => ({
       MapUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       MapOptions: {
         zoomSnap: 0.5
       },
+      pins: [
+        {
+          latlong: latLng(1.3340316734680562, 103.809814453125),
+          text: 'This is a marker text.'
+        }
+      ],
       MapAttr: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       map: {
         center: INITMAP.center,
